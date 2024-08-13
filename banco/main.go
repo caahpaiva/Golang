f244c21d7eda898fd737bece25f660523ae16575
CAROLINI PAIVA <carolini.damiao@sumup.com>
2024-08-13 14:56:54 -0300

    conta poupanca

--- banco/main.go
+++ banco/main.go
@@ -16,29 +16,33 @@ func main() {
 	// contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
 	// fmt.Println(contaDoBruno)
 
-	contaExemplo := contas.ContaCorrente{}
-	contaExemplo.Depositar(100)
+	// contaExemplo := contas.ContaCorrente{}
+	// contaExemplo.Depositar(100)
 
-	fmt.Println(contaExemplo.ObterSaldo())
+	// fmt.Println(contaExemplo.ObterSaldo())
+
+	// / contaDaSilvia := ContaCorrente{}
+	// contaDaSilvia.titular = "Silvia"
+	// contaDaSilvia.saldo = 500
+
+	// fmt.Println(contaDaSilvia.saldo)
+	// // contaDaSilvia.Depositar(500)
+	// status, valor := contaDaSilvia.Depositar(2000)
+	// fmt.Println(status, valor)
+
+	// fmt.Println(contaDaSilvia.Sacar(-100))
+	// fmt.Println(contaDaSilvia.saldo)
+	// contaDaSilvia := contas.ContaCorrenteContaCorrente{Titular: "Silvia", Saldo: 300}
+	// contaDoGustavo := contas.ContaCorrenteContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+	// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+
+	// fmt.Println(status)
+	// fmt.Println(contaDaSilvia)
+	// fmt.Println(contaDoGustavo)
+
+	contaDoDenis := contas.ContaPoupanca{}
+
+	fmt.Println(contaDoDenis)
 
 }
-
-// contaDaSilvia := ContaCorrente{}
-// contaDaSilvia.titular = "Silvia"
-// contaDaSilvia.saldo = 500
-
-// fmt.Println(contaDaSilvia.saldo)
-// // contaDaSilvia.Depositar(500)
-// status, valor := contaDaSilvia.Depositar(2000)
-// fmt.Println(status, valor)
-
-// fmt.Println(contaDaSilvia.Sacar(-100))
-// fmt.Println(contaDaSilvia.saldo)
-// contaDaSilvia := contas.ContaCorrenteContaCorrente{Titular: "Silvia", Saldo: 300}
-// contaDoGustavo := contas.ContaCorrenteContaCorrente{Titular: "Gustavo", Saldo: 100}
-
-// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-
-// fmt.Println(status)
-// fmt.Println(contaDaSilvia)
-// fmt.Println(contaDoGustavo)
