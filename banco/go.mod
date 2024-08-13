083e390e09be7b44c257111b08fc15ea84919e0c
CAROLINI PAIVA <carolini.damiao@sumup.com>
2024-08-13 14:45:22 -0300

    fix contaCorrente

---
+++ banco/go.mod
@@ -0,0 +1,5 @@
+module banco
+
+go 1.22.3
+
+require github.com/caahpaiva/Golang v0.0.0-20240813170724-ecb045688ba0 // indirect
--- banco/main.go
+++ banco/main.go
@@ -3,42 +3,42 @@ package main
 import (
 	"fmt"
 
-	"github.com/caahpaiva/Golang/tree/master/banco/contas"
-    "github.com/caahpaiva/Golang/tree/master/banco/clientes"
-
+	"github.com/caahpaiva/Golang/banco/contas"
 )
 
 func main() {
-		contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
-			Nome: "Bruno", 
-			CPF: "123.111.123.12",
-			Profissao: "Desenvolvedor"},
-			NumeroAgencia: 123, NumeroConta:123456, Saldo:100}
-			
-			fmt.Println(contaDoBruno)
+	// contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+	// 	Nome: "Bruno",
+	// 	CPF: "123.111.123.12",
+	// 	Profissao: "Desenvolvedor"},
+	// 	NumeroAgencia:123, NumeroConta:123456, Saldo:100}
+	// clienteBruno := clientes.Titular{"Bruno", "123.123.123.12", "Desenvolvedor Go"}
+	// contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
+	// fmt.Println(contaDoBruno)
 
-	}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
+	fmt.Println(contaExemplo.ObterSaldo())
 
-
-
-	// contaDaSilvia := ContaCorrente{}
-	// contaDaSilvia.titular = "Silvia"
-	// contaDaSilvia.saldo = 500
-
-	// fmt.Println(contaDaSilvia.saldo)
-	// // contaDaSilvia.Depositar(500)
-	// status, valor := contaDaSilvia.Depositar(2000)
-	// fmt.Println(status, valor)
-
-	// fmt.Println(contaDaSilvia.Sacar(-100))
-	// fmt.Println(contaDaSilvia.saldo)
-	// contaDaSilvia := contas.ContaCorrenteContaCorrente{Titular: "Silvia", Saldo: 300}
-	// contaDoGustavo := contas.ContaCorrenteContaCorrente{Titular: "Gustavo", Saldo: 100}
-
-	// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-
-	// fmt.Println(status)
-	// fmt.Println(contaDaSilvia)
-	// fmt.Println(contaDoGustavo)
 }
+
+// contaDaSilvia := ContaCorrente{}
+// contaDaSilvia.titular = "Silvia"
+// contaDaSilvia.saldo = 500
+
+// fmt.Println(contaDaSilvia.saldo)
+// // contaDaSilvia.Depositar(500)
+// status, valor := contaDaSilvia.Depositar(2000)
+// fmt.Println(status, valor)
+
+// fmt.Println(contaDaSilvia.Sacar(-100))
+// fmt.Println(contaDaSilvia.saldo)
+// contaDaSilvia := contas.ContaCorrenteContaCorrente{Titular: "Silvia", Saldo: 300}
+// contaDoGustavo := contas.ContaCorrenteContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+
+// fmt.Println(status)
+// fmt.Println(contaDaSilvia)
+// fmt.Println(contaDoGustavo)
